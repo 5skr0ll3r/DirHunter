43c7b63f290436f5bea9f0e33bd7be26aa44dc80
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2021-11-22 20:41:45 +0200

    Update dirhunter.py

--- dirhunter.py
+++ dirhunter.py
@@ -1,62 +1,54 @@
-'''
-MIT License
-
-Copyright (c) 2020 5skr0ll3r
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
-
 #!/usr/bin/python3
 
 import os
-import sys 
 import requests
+import argparse
 from termcolor import colored
 
 
 
 def main():
-    codes = ['100','101','102','103','200','201','202','203','204','205','206','207','226','300','301','302','403']
-    url = sys.argv[1]
-    wordlist = sys.argv[2]
-    exten = ('.html','.css','.js','.txt','.php','/')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u","--url",required=True)
+    parser.add_argument("-l","--wlist",required=True)
+    parser.add_argument("-c","--cert",default=False)
+    args = parser.parse_args()
+
     art()
-    lch = requests.get(url)
+
+    codes = ['100','101','102','103','200','201','202','203','204','205','206','207','226','300','301','302','403']
+    exten = ('.html','.css','.js','.txt','.php','/')
+
+
+    discoverer(args.url, args.wlist, args.cert, codes, exten)
+
+
+
+def discoverer(url, wlist, cert, codes, exten):
+    s = requests.Session()
+    lch = s.get(url, verify=cert)
     print('Url: ', url)
-    print('WrL: ', wordlist, '\n')
+    print('WordList: ', wlist, '\n')
     if str(lch.status_code) == '200':
-        if os.path.exists(wordlist):
-            with open(wordlist) as wl:
+        if os.path.exists(wlist):
+            with open(wlist) as wl:
                 for i in wl.readlines():
                     for x in exten: 
-                        if not i.startswith('#'):
+                        if not i.startswith('#') and not i == "\n": 
                             a = url + i.rstrip('\n') + x
-                            r = requests.get(a)
+                            r = s.get(a)
                             status = str(r.status_code)
                             if status in codes:
-                                arrow(a, status) # r
-                                print('\n')        
+                                arrow(a, status) # r       
         else:
             print("File Does Not Exist")
             
     else:
-        print("Host Not Responding Check Url")    
+        print("Host Not Responding Check the Url")    
+
+
+
 
 def art():
     

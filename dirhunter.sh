3545f322315c09eb46e79de9ff89dac28b270ad1
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2023-04-16 03:38:44 +0300

    Update dirhunter.sh

--- dirhunter.sh
+++ dirhunter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#set -x
+#MIT License at the bottom
+
 {
 extensions=('' ".html" ".css" ".js" ".txt" ".php" ".py" ".xml")
 ping_check=true
@@ -151,3 +152,26 @@ echo -e "\033[35m=============================\033[0m"
 
 echo -e "\n\033[32mAll child processes completed successfully.\033[0m\n"
 } 2>/dev/null
+
+
+#MIT License
+
+#Copyright (c) 2020 5skr0ll3r
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
